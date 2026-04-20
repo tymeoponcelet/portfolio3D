f463976f0742545d5f6e4641f6504a1e001932b1
unknown <tymeo.poncelet@gmail.com>
2026-04-20 05:01:33 +0200

    Fix: case sensitivity in base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portfolio3d/',
+  base: '/portfolio3D/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
